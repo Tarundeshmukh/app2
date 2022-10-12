23746ea842b7b8c3d15649d17a2c5a808b11b6e5
Tarundeshmukh <tarundeshmukh1997@gmail.com>
2022-10-12 09:50:07 +0530

    added-mock-list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Nagraj' },
+    { id: 13, name: 'Kali' },
+    { id: 14, name: 'Doga' },
+    { id: 15, name: 'Parmanu' },
+    { id: 16, name: 'Tenali' },
+    { id: 17, name: 'Goku' },
+    { id: 18, name: 'Baki' },
+    { id: 19, name: 'Kaneki' },
+    { id: 20, name: 'Zoro' }
+];
